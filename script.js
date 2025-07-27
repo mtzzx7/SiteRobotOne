191b9243d20c804e707621b6d65eb973b690d4e4
Matheus <matheusemanuel0567@gmail.com>
2025-07-27 02:13:14 -0300

    Dia 2
    Finalização completa das passagens de um site para o outro

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ const navList = document.querySelector('nav ul');
 toggleBtn.addEventListener('click', () => {
   navList.classList.toggle('active');
 });
+
+function toggleDropdown(event) {
+            event.stopPropagation(); // Impede que o clique no botão feche a dropdown
+            document.getElementById("myDropdown").classList.toggle("show");
+        }
+        // Fecha a dropdown se o usuário clicar fora dela
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropdown button')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                for (var i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
