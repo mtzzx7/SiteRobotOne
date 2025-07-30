1e525ffdb24a2e28c73543a35490fe66cc37fead
Matheus <matheusemanuel0567@gmail.com>
2025-07-29 21:57:21 -0300

    Atualização 29/07

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-        });
-      });
-    });
-const toggleBtn = document.querySelector('.menu-toggle');
-const navList = document.querySelector('nav ul');
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
 
-toggleBtn.addEventListener('click', () => {
-  navList.classList.toggle('active');
+  menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
 });
-
-function toggleDropdown(event) {
-            event.stopPropagation(); // Impede que o clique no botão feche a dropdown
-            document.getElementById("myDropdown").classList.toggle("show");
-        }
-        // Fecha a dropdown se o usuário clicar fora dela
-        window.onclick = function(event) {
-            if (!event.target.matches('.dropdown button')) {
-                var dropdowns = document.getElementsByClassName("dropdown-content");
-                for (var i = 0; i < dropdowns.length; i++) {
-                    var openDropdown = dropdowns[i];
-                    if (openDropdown.classList.contains('show')) {
-                        openDropdown.classList.remove('show');
-                    }
-                }
-            }
-        }
